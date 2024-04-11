644aa7ad665e38109c519aab15c62bd6239c7232
Deepak Tondle <deepak.tondle@gmail.com>
2024-04-11 11:51:22 +0530

    Parcel setup done

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const h1 = React.createElement('h1', {className: 'text-red'}, 'Hello World from React');
 const h2 = React.createElement('h2', {}, 'I am h2 tag');
 const child = React.createElement('div', {id: 'child'}, [h1, h2]);
---
+++ README.md
@@ -0,0 +1,2 @@
+# React JS Practice
+# Using Parcel
